fea9a64ecacef0a2784863532a78b3e320e89e33
tanehans <117091615+tanehans@users.noreply.github.com>
2023-04-19 22:59:31 +0300

    added song, zoom functionality and more images

---
+++ music.js
@@ -0,0 +1,18 @@
+var music = new Audio("Rasmus Gozzi - RID MIG SOM EN DALAHÄST.mp3");
+music.loop = true;
+
+
+$(document).ready(function() {
+    // add click handler to the music button
+    $("#music-button").click(function() {
+        if (music.muted) {
+            music.muted = false;
+            $(this).find("img").attr("src", "music.png");
+        } else {
+            music.muted = true;
+            $(this).find("img").attr("src", "mute.png");
+        }
+    });
+    // start the music
+    music.play();
+});
--- script.js
+++ script.js
@@ -1,45 +1,98 @@
-var imageUrls = [
-	"https://cdn.discordapp.com/attachments/284695168911671296/1098201239684128778/IMG_20230407_122927.jpg",
-	"https://cdn.discordapp.com/attachments/284695168911671296/1098200073105575936/IMG_20210611_200042.jpg",
-	"https://cdn.discordapp.com/attachments/284695168911671296/1098200072853913712/Snapchat-80256700.jpg",
-	"https://cdn.discordapp.com/attachments/284695168911671296/1098200072593875004/Snapchat-1822866671.jpg",
-	"https://cdn.discordapp.com/attachments/284695168911671296/1098200072329629706/Snapchat-927788833.jpg",
-	"https://cdn.discordapp.com/attachments/284695168911671296/1098200071465599056/Snapchat-1654282653.jpg",
-	"https://cdn.discordapp.com/attachments/284695168911671296/1098200071167811594/Snapchat-1545541308.jpg",
-	"https://cdn.discordapp.com/attachments/284695168911671296/1098200070878396466/Snapchat-193500899.jpg",
-];
+var imageUrls = [];
+
+$.ajax({
+  url: "bungaimg/",
+  success: function (data) {
+    $(data)
+      .find("a")
+      .attr("href", function (i, val) {
+        imageUrls.push(val);
+      });
+  },
+  async: false,
+});
 
 var maxImageWidth = 300;
 var maxImageHeight = 300;
 
 function createFallingImage() {
-	var imageUrl = imageUrls[Math.floor(Math.random() * imageUrls.length)];
-	var image = new Image();
-	image.onload = function() {
-		var aspectRatio = image.width / image.height;
-		var imageWidth = Math.min(Math.floor(Math.random() * maxImageWidth) + 150, image.width);
-		var imageHeight = Math.min(Math.floor(imageWidth / aspectRatio), maxImageHeight);
-		var imageElement = $("<img>")
-			.attr("src", imageUrl)
-			.css({
-				position: "absolute",
-				top: "-300px",
-				left: Math.random() * ($(window).width() - imageWidth),
-				"max-width": imageWidth,
-				"max-height": imageHeight
-			})
-			.appendTo("#falling-images")
-			.animate({
-				top: $(window).height() + "px",
-				left: "+=" + (Math.random() * 100 - 50) + "px"
-			}, Math.random() * 10000 + 5000, "linear", function() {
-				$(this).remove();
-			});
-	};
-	image.src = imageUrl;
-	setTimeout(createFallingImage, Math.random() * 1000);
+  var imageUrl = imageUrls[Math.floor(Math.random() * imageUrls.length)];
+  var image = new Image();
+  image.onload = function () {
+    var aspectRatio = image.width / image.height;
+    var imageWidth = Math.min(
+      Math.floor(Math.random() * maxImageWidth) + 150,
+      image.width
+    );
+    var imageHeight = Math.min(
+      Math.floor(imageWidth / aspectRatio),
+      maxImageHeight
+    );
+    var imageElement = $("<img>")
+      .attr("src", imageUrl)
+      .css({
+        position: "absolute",
+        top: "-300px",
+        left: Math.random() * ($(window).width() - imageWidth),
+        "max-width": imageWidth,
+        "max-height": imageHeight,
+        cursor: "pointer",
+      })
+      .appendTo("#falling-images")
+      .animate(
+        {
+          top: $(window).height() + "px",
+          left: "+=" + (Math.random() * 100 - 50) + "px",
+        },
+        Math.random() * 10000 + 5000,
+        "linear",
+        function () {
+          $(this).remove();
+        }
+      )
+      .click(function () {
+        var largeImage = $("<img>")
+          .attr("src", imageUrl)
+          .css({
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            "max-width": "80%",
+            "max-height": "80%",
+            cursor: "pointer",
+            "z-index": 9999,
+          })
+          .appendTo("body")
+          .click(function () {
+            $(this).remove();
+            $("#falling-images").css("opacity", 1.0);
+            overlay.remove();
+          });
+        var overlay = $("<div>")
+          .css({
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.8)",
+            "z-index": 9998,
+            cursor: "pointer",
+          })
+          .appendTo("body")
+          .click(function () {
+            largeImage.remove();
+            $("#falling-images").css("opacity", 1.0);
+            $(this).remove();
+          });
+        $("#falling-images").css("opacity", 0.5);
+      });
+  };
+  image.src = imageUrl;
+  setTimeout(createFallingImage, Math.random() * 1000);
 }
 
-$(document).ready(function() {
-	createFallingImage();
+$(document).ready(function () {
+  createFallingImage();
 });
