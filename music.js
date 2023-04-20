472e7771bec2f7a452c6c2094d1a29235853cf4f
tanehans <117091615+tanehans@users.noreply.github.com>
2023-04-21 00:34:12 +0300

    schubert :)

--- music.js
+++ music.js
@@ -1,19 +1,21 @@
-var music = new Audio("Rasmus Gozzi - RID MIG SOM EN DALAHÄST.mp3");
+var music = new Audio("./music.mp3");
 music.loop = true;
 
 $(document).one("click", function () {
   music.play();
+  //   change the volume to 0.1
+  music.volume = 0.3;
 });
 
-$(document).ready(function() {
-    // add click handler to the music button
-    $("#music-button").click(function() {
-        if (music.muted) {
-            music.muted = false;
-            $(this).find("img").attr("src", "music.png");
-        } else {
-            music.muted = true;
-            $(this).find("img").attr("src", "mute.png");
-        }
-    });
+$(document).ready(function () {
+  // add click handler to the music button
+  $("#music-button").click(function () {
+    if (music.muted) {
+      music.muted = false;
+      $(this).find("img").attr("src", "music.png");
+    } else {
+      music.muted = true;
+      $(this).find("img").attr("src", "mute.png");
+    }
+  });
 });
--- script.js
+++ script.js
@@ -4,14 +4,39 @@ var imageUrls = [];
 var maxImageWidth = 300;
 var maxImageHeight = 300;
 
+// Add video thumbnails to the imageUrls array
+var videoThumbnails = [
+  "Snapchat-628446483.jpg",
+  "Snapchat-1193473022.jpg",
+  "Snapchat-1315624666.jpg",
+  "Snapchat-1426759521.jpg",
+  "Snapchat-1870895666.jpg",
+  "Snapchat-2074205843.jpg",
+  "Snapchat-546945270.jpg",
+];
+
+// New helper function to check if the file name is a video thumbnail
+function isVideoThumbnail(fileName) {
+  return videoThumbnails.includes(fileName);
+}
+
+// New helper function to convert a thumbnail file name to its corresponding video file name
+function getVideoFileName(imageFileName) {
+  return imageFileName.replace(".jpg", ".mp4");
+}
+
 function createFallingImage() {
-  fetch("https://api.github.com/repos/erikbog/erikbog.github.io/contents/bungaimg?ref=main")
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(file => {
-          imageUrls.push(file.download_url);
+  fetch(
+    "https://api.github.com/repos/erikbog/erikbog.github.io/contents/bungaimg?ref=main"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((file) => {
+        imageUrls.push(file.download_url);
       });
       var imageUrl = imageUrls[Math.floor(Math.random() * imageUrls.length)];
+      var imageName = imageUrl.split("/").pop();
+      var isVideo = isVideoThumbnail(imageName);
       var image = new Image();
       image.onload = function () {
         var aspectRatio = image.width / image.height;
@@ -23,7 +48,8 @@ function createFallingImage() {
           Math.floor(imageWidth / aspectRatio),
           maxImageHeight
         );
-        var imageElement = $("<img>")
+
+        mediaElement = $("<img>")
           .attr("src", imageUrl)
           .css({
             position: "absolute",
@@ -32,7 +58,9 @@ function createFallingImage() {
             "max-width": imageWidth,
             "max-height": imageHeight,
             cursor: "pointer",
-          })
+          });
+
+        mediaElement
           .appendTo("#falling-images")
           .animate(
             {
@@ -46,24 +74,50 @@ function createFallingImage() {
             }
           )
           .click(function () {
-            var largeImage = $("<img>")
-              .attr("src", imageUrl)
-              .css({
-                position: "fixed",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                "max-width": "80%",
-                "max-height": "80%",
-                cursor: "pointer",
-                "z-index": 9999,
-              })
-              .appendTo("body")
-              .click(function () {
-                $(this).remove();
-                $("#falling-images").css("opacity", 1.0);
-                overlay.remove();
-              });
+            var largeMedia;
+            if (isVideo) {
+              var videoFileName = getVideoFileName(imageName);
+              largeMedia = $("<video>")
+                .attr("src", "bungavid/" + videoFileName)
+                .attr("autoplay", true)
+                .attr("loop", true)
+                .prop("controls", false)
+                .css({
+                  position: "fixed",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  "max-width": "80%",
+                  "max-height": "80%",
+                  cursor: "pointer",
+                  "z-index": 9999,
+                })
+                .appendTo("body")
+                .click(function () {
+                  $(this).remove();
+                  $("#falling-images").css("opacity", 1.0);
+                  overlay.remove();
+                });
+            } else {
+              largeMedia = $("<img>")
+                .attr("src", imageUrl)
+                .css({
+                  position: "fixed",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  "max-width": "80%",
+                  "max-height": "80%",
+                  cursor: "pointer",
+                  "z-index": 9999,
+                })
+                .appendTo("body")
+                .click(function () {
+                  $(this).remove();
+                  $("#falling-images").css("opacity", 1.0);
+                  overlay.remove();
+                });
+            }
             var overlay = $("<div>")
               .css({
                 position: "fixed",
@@ -77,18 +131,19 @@ function createFallingImage() {
               })
               .appendTo("body")
               .click(function () {
-                largeImage.remove();
+                largeMedia.remove();
                 $("#falling-images").css("opacity", 1.0);
                 $(this).remove();
               });
             $("#falling-images").css("opacity", 0.5);
           });
-      };
-      image.src = imageUrl;
-      setTimeout(createFallingImage, Math.random() * 1000);
-    });
-}
-
-$(document).ready(function () {
-  createFallingImage();
-});
+        };
+  
+        image.src = imageUrl;
+        setTimeout(createFallingImage, Math.random() * 1000);
+      });
+  }
+  
+  $(document).ready(function () {
+    createFallingImage();
+  });
